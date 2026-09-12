4c4b69a09d965a21ba173891785ba39e2d5d47c3
triplebbbaka-debug <triplebbbaka@gmail.com>
2026-09-12 17:52:05 -0400

    Starting Autotileing

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -3,20 +3,25 @@ extends CharacterBody2D
 
 #Establishes direction
 var direction: Vector2
-var current_tool: Enum.Tool = Enum.Tool.AXE
+var can_move: bool = true
+var current_tool: Enum.Tool
+var current_seed: Enum.Seed
 var speed := 50
 @onready var move_state_machine = $Animation/AnimationTree.get("parameters/MoveStateMachine/playback")
 @onready var tool_state_machine = $Animation/AnimationTree.get("parameters/ToolStateMachine/playback")
 func _physics_process(_delta: float) -> void:
-	get_basic_input()
-	move()
-	animate()
+	if can_move:
+		get_basic_input()
+		move()
+		animate()
 #Handles movement
 
 func get_basic_input():
 	if Input.is_action_just_pressed("tool_backward") or Input.is_action_just_pressed("tool_forward"):
 		var dir = Input.get_axis("tool_backward", "tool_forward")
-		current_tool = posmod(current_tool + int(dir), Enum.Tool.size())
+		current_tool = posmod(current_tool + int(dir), Enum.Tool.size()) as Enum.Tool
+	if Input.is_action_just_pressed("seed_forward"):
+		current_seed = posmod(current_seed + 1, Enum.Seed.size()) as Enum.Seed
 	if Input.is_action_just_pressed("action"):
 		tool_state_machine.travel(Data.TOOL_STATE_ANIMATIONS[current_tool])
 		$Animation/AnimationTree.set("parameters/ToolOneShot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
@@ -37,3 +42,8 @@ func animate():
 		move_state_machine.travel('Idle')
 func tool_use_emit():
 	print('tool')
+
+func _on_animation_tree_animation_started(_anim_name: StringName) -> void:
+	can_move = false
+func _on_animation_tree_animation_finished(_anim_name: StringName) -> void:
+	can_move = true
