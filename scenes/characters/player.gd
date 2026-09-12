b159881e68a22aebe5cbd9227d1f249051ec76c6
triplebbbaka-debug <triplebbbaka@gmail.com>
2026-09-12 13:04:31 -0400

    First Challenge
    At 27:38, the viewer is given a challenge to make the player's animation match the direction. This is my try.

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -1,4 +1,28 @@
 extends CharacterBody2D
+#Player Script
 
+#Establishes direction
+var direction: Vector2
+
+var speed := 50
+
+func _physics_process(_delta: float) -> void:
+	move()
+	animate()
+#Handles movement
+func move():
+	direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * speed
+	move_and_slide()
+
+func animate():
+	if direction.x == 1:
+		$Animation/AnimationTree.set("parameters/MoveStateMachine/Walk/blend_position", Vector2.RIGHT)
+	elif direction.x == -1:
+		$Animation/AnimationTree.set("parameters/MoveStateMachine/Walk/blend_position", Vector2.LEFT)
+	elif direction.y == -1:
+		$Animation/AnimationTree.set("parameters/MoveStateMachine/Walk/blend_position", Vector2.UP)
+	elif direction.y == 1:
+		$Animation/AnimationTree.set("parameters/MoveStateMachine/Walk/blend_position", Vector2.DOWN)
 func tool_use_emit():
 	print('tool')
