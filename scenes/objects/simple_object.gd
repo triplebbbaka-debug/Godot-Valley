85f2bfd8f96d530776b3eb27f9c0dbef23409127
triplebbbaka-debug <triplebbbaka@gmail.com>
2026-09-24 21:49:25 -0400

    Finsihed Editor Tools

---
+++ scenes/objects/simple_object.gd
@@ -0,0 +1,25 @@
+@tool
+extends StaticBody2D
+
+@export_range(0,3,1) var size: int:
+	set(value):
+		size = value
+		$Sprite2D.frame_coords = Vector2i(size, style)
+@export_enum("Bush", "Rock") var style: int:
+	set(value):
+		style = value
+		$Sprite2D.frame_coords = Vector2i(size, style)
+@export var random: bool
+
+@export_tool_button("Randomize", "Callable") var randimizer = randomize 
+func _ready() -> void:
+	if random:
+		size = randi_range(0, $Sprite2D.hframes - 1)
+		style = [0,1].pick_random()
+	$Sprite2D.frame_coords = Vector2i(size, style)
+	$CollisionShape2D.disabled = size < 2
+	z_index = -1 if size < 2 else 0
+func randomize():
+	size = randi_range(0, $Sprite2D.hframes - 1)
+	style = [0,1].pick_random()
+	$Sprite2D.frame_coords = Vector2i(size, style)
